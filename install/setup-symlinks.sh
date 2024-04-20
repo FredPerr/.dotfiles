cb984a82720801cd45c364cdc6aaf159fd37e27a
Frédéric Perron <frederic.perr@gmail.com>
2024-04-20 15:51:17 -0400

    feat(macos) add basic config

--- install/setup-symlinks.sh
+++ install/setup-symlinks.sh
@@ -5,23 +5,23 @@ if  [ ! "$EUID" -eq 0 ]; then
 fi
 HOME="/home/${SUDO_USER}"
 
-ln -sf ~/.dotfiles/config/.bashrc ~/.bashrc
-ln -sf ~/.dotfiles/config/.zshrc ~/.zshrc
-ln -sf ~/.dotfiles/config/.zsh_functions ~/.zsh_functions
-ln -sf ~/.dotfiles/config/.zshenv ~/.zshenv
-ln -sf ~/.dotfiles/assets/.fonts ~/.fonts
+ln -sf $HOME/.dotfiles/config/.bashrc $HOME/.bashrc
+ln -sf $HOME/.dotfiles/config/.zshrc $HOME/.zshrc
+ln -sf $HOME/.dotfiles/config/.zsh_functions $HOME/.zsh_functions
+ln -sf $HOME/.dotfiles/config/.zshenv $HOME/.zshenv
+ln -sf $HOME/.dotfiles/assets/.fonts $HOME/.fonts
 
 #
-# Inside ~/.config/ folder
+# Inside $HOME/.config/ folder
 #
 
-ln -sf ~/.dotfiles/config/alacritty ~/.config/alacritty
-ln -sf ~/.dotfiles/config/autostart ~/.config/autostart
-ln -sf ~/.dotfiles/config/i3 ~/.config/i3
-ln -sf ~/.dotfiles/config/nvim ~/.config/nvim
-ln -sf ~/.dotfiles/config/picom ~/.config/picom
-ln -sf ~/.dotfiles/config/tmux ~/.config/tmux
-ln -sf ~/.dotfiles/config/autorandr ~/.config/autorandr
-ln -sf ~/.dotfiles/config/monitors.xml ~/.config/monitors.xml
+ln -sf $HOME/.dotfiles/config/alacritty $HOME/.config/alacritty
+ln -sf $HOME/.dotfiles/config/autostart $HOME/.config/autostart
+ln -sf $HOME/.dotfiles/config/i3 $HOME/.config/i3
+ln -sf $HOME/.dotfiles/config/nvim $HOME/.config/nvim
+ln -sf $HOME/.dotfiles/config/picom $HOME/.config/picom
+ln -sf $HOME/.dotfiles/config/tmux $HOME/.config/tmux
+ln -sf $HOME/.dotfiles/config/autorandr $HOME/.config/autorandr
+ln -sf $HOME/.dotfiles/config/monitors.xml $HOME/.config/monitors.xml
 
 echo "Setup symlinks successfully!"
