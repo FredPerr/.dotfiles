b682f1114fe4e7d0988f9b13d8b3c7187f2a5921
Frédéric Perron <frederic.perr@gmail.com>
2026-02-08 16:20:09 -0500

    feat: upgrade aliases

--- config/aliases.sh
+++ config/aliases.sh
@@ -1,19 +1,26 @@
-# Project Selector UI
 p() {
-  # 1. Find directories in ~/projects (1 level deep)
-  # 2. Pipe into fzf with a nice layout
-  # 3. Store the selection in a variable
-  local project=$(fd . ~/projects --max-depth 1 --type d --exec basename | fzf --height 40% --layout=reverse --border --prompt="📂 Project > " --header="Select a project to open in VS Code")
+  # 1. fd: Find directories, ignore those starting with _, depth 1
+  # 2. perl: Colorize the prefix (before _ or -) with Cyan
+  # 3. fzf: Display beautiful UI with --ansi to support perl colors
+  local project=$(fd . ~/projects --max-depth 1 --type d --exclude "_*" --exec basename | perl -pe '
+    # Colorize prefix before _ or - in Cyan (\e[36m)
+    if (s/^([^_|-]+)([_|-])/\e[36m$1\e[0m$2/) { } 
+    # Colorize standalone projects in Green (\e[32m)
+    else { s/^([a-zA-Z0-9]+)/\e[32m$1\e[0m/ }
+  ' | fzf --height 40% \
+          --ansi \
+          --layout=reverse \
+          --border \
+          --prompt="📂 Project > " \
+          --header="Search projects (Excluding _*)")
 
-  # If a selection was made
-  if [ -n "$project" ]; then
-    local project_path="$HOME/projects/$project"
-    
-    echo "Opening $project..."
-    # Open in VS Code
+  # Strip ANSI color codes before using the variable
+  local clean_project=$(echo "$project" | sed 's/\x1b\[[0-9;]*m//g')
+
+  if [ -n "$clean_project" ]; then
+    local project_path="$HOME/projects/$clean_project"
+    echo "Opening $clean_project..."
     code "$project_path"
-    
-    # Optional: Also navigate your terminal there
     cd "$project_path"
   fi
 }
